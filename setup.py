ff6585483e1756f3aa05b9b0f87bcd5c6ce16569
Ben Farr <bfarr@uoregon.edu>
2018-03-08 12:40:19 -0800

    Bump to v1.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name='prism_plot',
-    version='1.3',
+    version='1.4',
     description="An animated wrapping of Dan Foreman-Mackey's triangle.py.",
     author='Ben Farr',
     author_email='farr@uchicago.edu',
